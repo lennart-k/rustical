14328e6f943882688646d69c11515d6754413091
Lennart <18233294+lennart-k@users.noreply.github.com>
2024-06-01 14:42:14 +0200

    Remove now unnecessary generate_multistatus function

--- crates/dav/src/xml_snippets.rs
+++ crates/dav/src/xml_snippets.rs
@@ -1,5 +1,3 @@
-use anyhow::Result;
-use quick_xml::{events::attributes::Attribute, Writer};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -14,22 +12,3 @@ impl HrefElement {
 
 #[derive(Serialize)]
 pub struct TextNode(pub Option<String>);
-
-pub fn generate_multistatus<'a, F, A>(namespaces: A, closure: F) -> Result<String>
-where
-    F: FnOnce(&mut Writer<&mut Vec<u8>>) -> Result<(), quick_xml::Error>,
-    A: IntoIterator,
-    A::Item: Into<Attribute<'a>>,
-{
-    let mut output_buffer = Vec::new();
-    let mut writer = Writer::new_with_indent(&mut output_buffer, b' ', 2);
-    writer
-        .create_element("multistatus")
-        .with_attributes(namespaces)
-        .write_inner_content(closure)?;
-
-    Ok(format!(
-        "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n{}",
-        std::str::from_utf8(&output_buffer)?
-    ))
-}
