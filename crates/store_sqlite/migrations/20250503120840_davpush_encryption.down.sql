d0c3d8f25819a24d72206c22658261c90a0db603
Lennart <18233294+lennart-k@users.noreply.github.com>
2025-05-06 14:25:03 +0200

    sqlite: Fix down-migration

--- crates/store_sqlite/migrations/20250503120840_davpush_encryption.down.sql
+++ crates/store_sqlite/migrations/20250503120840_davpush_encryption.down.sql
@@ -1,4 +1,3 @@
-ALTER TABLE davpush_subscriptions
-DROP public_key,
-DROP public_key_type,
-DROP auth_secret;
+ALTER TABLE davpush_subscriptions DROP public_key;
+ALTER TABLE davpush_subscriptions DROP public_key_type;
+ALTER TABLE davpush_subscriptions DROP auth_secret;
