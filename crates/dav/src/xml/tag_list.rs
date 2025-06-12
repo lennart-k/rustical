5a3a2c090977f27d1b43e5e5a404d4149f3f9711
Lennart <18233294+lennart-k@users.noreply.github.com>
2025-06-12 16:18:33 +0200

    Fix TagList not writing the <prop> wrapper

--- crates/dav/src/xml/tag_list.rs
+++ crates/dav/src/xml/tag_list.rs
@@ -1,5 +1,8 @@
 use derive_more::derive::From;
-use quick_xml::name::Namespace;
+use quick_xml::{
+    events::{BytesEnd, BytesStart, Event},
+    name::Namespace,
+};
 use rustical_xml::{NamespaceOwned, XmlSerialize};
 use std::collections::HashMap;
 
@@ -9,17 +12,47 @@ pub struct TagList(Vec<(Option<NamespaceOwned>, String)>);
 impl XmlSerialize for TagList {
     fn serialize<W: std::io::Write>(
         &self,
-        _ns: Option<Namespace>,
-        _tag: Option<&[u8]>,
-        _namespaces: &HashMap<Namespace, &[u8]>,
+        ns: Option<Namespace>,
+        tag: Option<&[u8]>,
+        namespaces: &HashMap<Namespace, &[u8]>,
         writer: &mut quick_xml::Writer<W>,
     ) -> std::io::Result<()> {
+        let prefix = ns
+            .map(|ns| namespaces.get(&ns))
+            .unwrap_or(None)
+            .map(|prefix| {
+                if !prefix.is_empty() {
+                    [*prefix, b":"].concat()
+                } else {
+                    Vec::new()
+                }
+            });
+        let has_prefix = prefix.is_some();
+        let tagname = tag.map(|tag| [&prefix.unwrap_or_default(), tag].concat());
+        let qname = tagname
+            .as_ref()
+            .map(|tagname| ::quick_xml::name::QName(tagname));
+
+        if let Some(qname) = &qname {
+            let mut bytes_start = BytesStart::from(qname.to_owned());
+            if !has_prefix {
+                if let Some(ns) = &ns {
+                    bytes_start.push_attribute((b"xmlns".as_ref(), ns.as_ref()));
+                }
+            }
+            writer.write_event(Event::Start(bytes_start))?;
+        }
+
         for (ns, tag) in &self.0 {
             let mut el = writer.create_element(tag);
             if let Some(ns) = ns {
                 el = el.with_attribute(("xmlns", String::from_utf8_lossy(&ns.0)));
             }
             el.write_empty()?;
+        }
+
+        if let Some(qname) = &qname {
+            writer.write_event(Event::End(BytesEnd::from(qname.to_owned())))?;
         }
         Ok(())
     }
