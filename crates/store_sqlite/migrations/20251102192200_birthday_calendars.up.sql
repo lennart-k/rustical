547e477ecaa8cf1a419a677b5e1669b28f16b0f6
Lennart <18233294+lennart-k@users.noreply.github.com>
2025-11-02 21:05:31 +0100

    make sure a birthday calendar will be created for each addressbook

--- crates/store_sqlite/migrations/20251102192200_birthday_calendars.up.sql
+++ crates/store_sqlite/migrations/20251102192200_birthday_calendars.up.sql
@@ -12,4 +12,15 @@ CREATE TABLE birthday_calendars (
     CONSTRAINT fk_birthdays_addressbooks FOREIGN KEY (principal, id)
     REFERENCES addressbooks (principal, id) ON DELETE CASCADE
     -- birthday calendar stores no meaningful data so we can cascade
-)
+);
+
+INSERT INTO birthday_calendars
+(principal, id, displayname, deleted_at, push_topic)
+SELECT
+    principal,
+    id,
+    displayname || ' birthdays' AS displayname,
+    deleted_at,
+    push_topic || substr(printf('%d', random()), -4) AS push_topic
+    -- jank suffix to ensure that new push_topic is different :D
+FROM addressbooks;
