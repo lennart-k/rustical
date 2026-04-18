8e4560968993086981aa7d5a2d770b1abb1f79c3
Lennart <18233294+lennart-k@users.noreply.github.com>
2026-04-18 15:11:20 +0200

    davpush: Delete expired subscriptions

--- crates/store/src/subscription_store.rs
+++ crates/store/src/subscription_store.rs
@@ -1,15 +1,24 @@
 use crate::Error;
 use async_trait::async_trait;
-use chrono::NaiveDateTime;
+use chrono::{DateTime, NaiveDateTime, TimeZone};
 
 pub struct Subscription {
     pub id: String,
     pub topic: String,
+    // Naive because sqlite has no concept of timezones
+    // In reality, this is UTC
     pub expiration: NaiveDateTime,
     pub push_resource: String,
     pub public_key: String,
     pub public_key_type: String,
     pub auth_secret: String,
+}
+
+impl Subscription {
+    #[must_use]
+    pub fn is_expired(&self, now: &DateTime<impl TimeZone>) -> bool {
+        self.expiration < now.naive_utc()
+    }
 }
 
 #[async_trait]
