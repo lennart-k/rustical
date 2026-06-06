4dfc0d1143cc85e1cbc5389eb987c7c1e21d116f
Lennart Kämmle <18233294+lennart-k@users.noreply.github.com>
2026-06-06 20:41:51 +0200

    Support UNIX sockets in health command

--- src/commands/health.rs
+++ src/commands/health.rs
@@ -9,11 +9,18 @@ pub struct HealthArgs {}
 /// Currently just pings to see if it's reachable via HTTP
 #[allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 pub async fn cmd_health(http_config: HttpConfig, _health_args: HealthArgs) -> anyhow::Result<()> {
-    let client = reqwest::ClientBuilder::new().build().unwrap();
+    let bind_config = http_config.bind_config()?;
+    let mut client_builder = reqwest::ClientBuilder::new();
 
-    let HttpBindConfig::Tcp(address) = http_config.bind_config()? else {
-        todo!("Listening on UNIX sockets not implemented yet");
+    let address = match bind_config {
+        HttpBindConfig::Tcp(address) => address,
+        HttpBindConfig::Unix(path) => {
+            client_builder = client_builder.unix_socket(path);
+            "rustical".to_string()
+        }
     };
+    let client = client_builder.build()?;
+
     let endpoint = format!("http://{address}/ping").parse().unwrap();
     let request = reqwest::Request::new(Method::GET, endpoint);
 
